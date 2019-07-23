edc449cfc1d968189ade0189e826f2af50e56d4f
Salih Ozkan <it.salih.ozkan@gmail.com>
2019-07-23 23:19:03 +0200

    Added login system, tag deletion and displaying how many articles use the tag.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,28 @@
 class TagsController < ApplicationController
+	before_action :require_login, only: [:destroy]
+
+	def require_login
+		if logged_in?
+			return true
+		else
+			redirect_to login_path
+		end
+	end
+
 	def index
 		@tags = Tag.all
 	end
 
 	def show
 		@tag = Tag.find(params[:id])
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		deletedTagName = @tag.name
+		flash.notice = "The tag(#{deletedTagName}) was deleted."
+		@tag.destroy
+
+		redirect_to tags_path
 	end
 end
